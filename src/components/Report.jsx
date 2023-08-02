5f5737fc72288a978238995ffbc5b9df0fdf15d2
elsie <elsiechen915@gmail.com>
2023-08-01 21:44:51 -0400

    Finish head in report

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,10 +1,39 @@
+import emailImg from '/email.png'
+import phoneImg from '/phone.png'
+import addressImg from '/address.png'
+
 export default function Report({general}) {
     return (
-        <>
-            <h1>{general.fullName}</h1>
-            <div>
-                {general.email}{general.phone}{general.address}
+        <div className="report-container">
+            <div className="head">
+                <h1>{general.fullName}</h1>
+                <div>
+                    <div className='contactDiv'>
+                        <img src={emailImg} alt="Email" />
+                        <div>{general.email}</div>
+                    </div>
+                    <div className='contactDiv'>
+                        <img src={phoneImg} alt="Phone" />
+                        <div>{general.phone}</div>
+                    </div>
+                    <div className='contactDiv'>
+                        <img src={addressImg} alt="Address" />
+                        <div>{general.address}</div>
+                    </div>
+                </div>
             </div>
-        </>
+            <div className="education">
+
+            </div>
+            <div className="experience">
+
+            </div>
+            <div className="skill">
+
+            </div>
+            <div className="certificate">
+
+            </div>
+        </div>
     )
 }
