8e273a243f538439be34fea75a305a446e8ed868
elsie <elsiechen915@gmail.com>
2023-08-02 22:36:03 -0400

    Delete unnecessary commented-out code

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -14,19 +14,6 @@ export default function EducationForm(
             <h3>Education</h3>
             {educations.map(education => (
                 <form key={education.id}>
-                    {/* <div className="field">
-                        <label htmlFor='school'>School</label>
-                        <input value={education.school}
-                                onChange={(e) => handleChange(e, education.id)}
-                                id='school'/>
-                    </div>
-                    <div className="field">
-                        <label htmlFor='degree'>Degree</label>
-                        <input value={education.degree}
-                                onChange={(e) => handleChange(e, education.id)}
-                                id='degree'/>
-                    </div> */}
-                    
                     {console.log(education['startDate'])}
                     {console.log(education['endDate'])}
                     {keyItems.map(keyItem => (
